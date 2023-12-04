5e8d314b77da233d8f9356859a6aa615ca950fcf
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-03 23:44:18 -0500

    Add grid and hover function
    created 16x16 grid with flexbox style and add function to
    recognize on hover it will change style of border color.

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+
+for (let n = 0; n<16; n++) {
+    let gridLine = document.createElement('div');
+    gridLine.classList.add('gridLine');
+    container.appendChild(gridLine);
+    for (let i = 0; i<16; i++) {
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('gridBox');
+        gridLine.appendChild(gridBox)
+
+    }
+}
+
+let grid = document.querySelectorAll('.gridBox');
+grid.forEach((element) => element.addEventListener('mouseover', () => element.style.borderColor = 'red'))
