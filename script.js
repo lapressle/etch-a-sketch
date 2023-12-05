055ed033fb3429710e4ddb838705c7654f7bb9b9
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-04 23:35:00 -0500

    Add button to adjust grid size
    Button will adjust to x by x grid but not square yet.

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const container = document.querySelector('#container');
 
-for (let n = 0; n<16; n++) {
-    let gridLine = document.createElement('div');
-    gridLine.classList.add('gridLine');
-    container.appendChild(gridLine);
-    for (let i = 0; i<16; i++) {
-        let gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
-        gridLine.appendChild(gridBox)
-
+function makeGrid (gridSize) {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+    for (let n = 0; n<gridSize; n++) {
+        let gridLine = document.createElement('div');
+        gridLine.classList.add('gridLine');
+        let setHeight = 960/gridSize;
+        let setWidth = 960/gridSize;
+        gridLine.style.minHeight = setHeight+'px';
+        gridLine.style.minWidth = setHeight+'px';
+        container.appendChild(gridLine);
+        for (let i = 0; i<16; i++) {
+            let gridBox = document.createElement('div');
+            gridBox.classList.add('gridBox');
+            gridLine.appendChild(gridBox)
+    
+        }
     }
+    let grid = document.querySelectorAll('.gridBox');
+    grid.forEach((element) => element.addEventListener('mouseover', () => element.style.backgroundColor = 'red'))
+    grid.forEach((element) => element.addEventListener('mouseout', () => element.style.borderColor = 'black'))
 }
 
+makeGrid(22);
+
+const gridButton = document.querySelector('button');
+
+gridButton.addEventListener('click', () => makeGrid(prompt('What size would you like?')));
+
 let grid = document.querySelectorAll('.gridBox');
-grid.forEach((element) => element.addEventListener('mouseover', () => element.style.borderColor = 'red'))
+grid.forEach((element) => element.addEventListener('mouseover', () => element.style.backgroundColor = 'red'))
 grid.forEach((element) => element.addEventListener('mouseout', () => element.style.borderColor = 'black'))
