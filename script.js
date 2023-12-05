ea380aaa27ddb3ca321016b9ba3474465dd1b162
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-05 00:12:01 -0500

    Add limit to gridSize
    If gridSize is too large (>100), warning is displayed and action
    cancelled to prevent getting caught with memory usage.

--- script.js
+++ script.js
@@ -10,25 +10,30 @@ function randomRGB() {
 }
 
 function makeGrid (gridSize) {
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
-      }
-    for (let n = 0; n<gridSize; n++) {
-        let gridLine = document.createElement('div');
-        gridLine.classList.add('gridLine');
-        let setHeight = 960/gridSize;
-        let setWidth = 960/gridSize;
-        gridLine.style.minHeight = setHeight+'px';
-        container.appendChild(gridLine);
-        for (let i = 0; i<gridSize; i++) {
-            let gridBox = document.createElement('div');
-            gridBox.classList.add('gridBox');
-            gridLine.appendChild(gridBox)
-    
-        }
+    if (gridSize > 100) {
+        alert('Sorry that is too large!');
+    }   else {
+            while (container.firstChild) {
+                container.removeChild(container.lastChild);
+            }
+            for (let n = 0; n<gridSize; n++) {
+                let gridLine = document.createElement('div');
+                gridLine.classList.add('gridLine');
+                let setHeight = 960/gridSize;
+                let setWidth = 960/gridSize;
+                gridLine.style.minHeight = setHeight+'px';
+                container.appendChild(gridLine);
+                for (let i = 0; i<gridSize; i++) {
+                    let gridBox = document.createElement('div');
+                    gridBox.classList.add('gridBox');
+                    gridLine.appendChild(gridBox)
+            
+                }
+            }
+            let grid = document.querySelectorAll('.gridBox');
+            grid.forEach((element) => element.addEventListener('mouseover', () => element.style.backgroundColor = randomRGB()))
     }
-    let grid = document.querySelectorAll('.gridBox');
-    grid.forEach((element) => element.addEventListener('mouseover', () => element.style.backgroundColor = randomRGB()))
+
 }
 
 makeGrid(22);
